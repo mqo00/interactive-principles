5a1ee86d239e86b1e905da86290c96f8244e5a46
Katie McTigue <kaitlinmctigue@gmail.com>
2019-07-11 00:35:46 -0400

    error handling for game ex image

--- src/components/GameExImage.js
+++ src/components/GameExImage.js
@@ -1,47 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-// import Lightbox from 'react-image-lightbox';
 
-class GameExImg extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            lightboxOpen: false,
-            img: ''
-        };
-
-        this.setImage = this.setImage.bind(this);
-
-        // this.setImage();
-    }
-
-    componentDidMount() {
-        this.setImage();
-    }
-
-    // componentDidUpdate() {
-    //     this.setImage();
-    // }
-
-    setImage() {
-        this.setState({img: require('../images/game_examples/' + (this.props.id) + '.png')});
-    }
-
-    render() {
-        return (
-            <div style={{backgroundImage: 'url(' + this.state.img + ')'}}
-                className={'img img-responsive ' + this.props.classes}
-                onClick={() => this.setState({ lightboxOpen: true })}
-            >
-            </div>
-        );
-    }
+function GameExImage(props) {
+    var image;
+    try {image = require('../images/game_examples/' + (props.id) + '.png');}
+    catch(e) {console.log(e);}
+    return (
+        image ? <div style={{backgroundImage: 'url(' + image + ')'}} className={'principle-image ' + props.classes}></div> : ''
+    );
 }
 
-GameExImg.propTypes = {
+GameExImage.propTypes = {
     id: PropTypes.any,
     classes: PropTypes.any
 };
 
-export default GameExImg;
+export default GameExImage;
