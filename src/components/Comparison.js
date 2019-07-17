87c07354dd5c5ad0f11a2df7ff02e35564f0a3a5
Katie McTigue <kaitlinmctigue@gmail.com>
2019-07-16 23:26:09 -0400

    updates principles. fix to comparison

--- src/components/Comparison.js
+++ src/components/Comparison.js
@@ -6,21 +6,30 @@ function Comparison(props) {
     var parts;
     var item1;
     var item2;
+    var item3;
     var reverse = false;
     var nocomparison = false;
 
     example1 = (props.comparison.match(/\n/)) ? (props.comparison.split(/\n/)[0]) : props.comparison;
 
     if (example1.includes('>')) {
+        //standard greater than comparison
         parts = example1.split('>');
         item1 = parts[0];
         item2 = parts[1];
+
+        //has a third one
+        if(parts[2]) {
+            item3 = parts[2];
+        }
     } else if (example1.includes('<')) {
+        //standard less than comparison
         parts = example1.split('<');
         item1 = parts[1];
         item2 = parts[0];
         reverse = true;
     } else {
+        //not a comparison, just an example
         nocomparison = true;
     }
 
@@ -36,6 +45,16 @@ function Comparison(props) {
         return (
             <div className={'comparison'}>
                 <span className={'comparison__greater'}>{example1} </span>
+            </div>
+        );
+    } else if (item3) {
+        return(
+            <div className={'comparison'}>
+                <span className={'comparison__greater'}>{item1} </span>
+                <span className={'comparison__icon fas fa-greater-than'}></span>
+                <span className={'comparison__less'}> {item2}</span>
+                <span className={'comparison__icon fas fa-greater-than'}></span>
+                <span className={'comparison__less'}> {item3}</span>
             </div>
         );
     } else {
