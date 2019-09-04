5b519fb05e645e5a528fe1aa17ccd4d8b6703554
eharpste <eharpste@cs.cmu.edu>
2019-09-04 12:22:17 -0400

    Added links to the LearnLab and expanded the About page a bit

--- src/components/About.js
+++ src/components/About.js
@@ -22,28 +22,38 @@ function About() {
             <h1 className={'about__title'}>About</h1>
             <div className={'about__section'}>
                 <p className={'about__section__paragraph'}>
-                    The mission of the Interactive Principles project is help make research backed principles
-                    from the learning sciences more usable for designers of instructional technologies, specifically
-                    educational games, by providing examples and contextualizing the design questions that may have
-                    pedagogical impacts. The current site is used in Carnegie Mellon University’s 05-418/818 Design
-                    of Educational Games course as a tool to help students in exploring and working with
-                    principles covered in the course.
+                    The mission of the Interactive Principles project is help
+                    make research backed principles from the learning sciences
+                    more understandable and usable for designers of
+                    instructional technologies and experiences, specifically
+                    educational games. We do this by providing examples and
+                    contextualizing the design questions that can have
+                    pedagogical impacts. This current site is primarily design
+                    for use in Carnegie Mellon University’s 05-418/818 Design
+                    of Educational Games course (<a className={'link'} href={'https://edugames.design'} target={'_blank'}>https://edugames.design</a>) as a tool to
+                    help students in learning, exploring, and working with principles
+                    covered in the course.
                 </p>
                 <p className={'about__section__paragraph'}>
-                    The current set of 30 principles is from
-                    Koedinger, Booth, and Klahr’s article Instructional Complexity and the Science to Constrain It,
-                    in which the authors summarized a number of different principle sets into a concise set of 30.
+                    The current set of 30 principles shown on the site is from Koedinger, Booth, and Klahr’s 2013 article Instructional
+                    Complexity and the Science to Constrain It [1], in which the authors summarized a number of different principle sets
+                    into a concise set of 30.
+                </p>
+                <p className={'about__section__paragraph'}>
+                    Many other sets of Learning Science Principles have been proposed in the literature. For a listing of several
+                    existing taxonomies take a look at the <a className={'link'} href={'https://learnlab.org/research/wiki/Main_Page'} target={'_blank'}>LearnLab Theory Wiki</a>. If you are a
+                    researcher or know about additional sets of principles that you think could be incorporated into the project feel free to reach out to us.              
                 </p>
             </div>
 
-            <h2 className={'about__subtitle'}>Works Cited</h2>
+            <h2 className={'about__subtitle'}>References</h2>
             <div className={'about__section'}>
                 <div className={'about__section__paragraph about__section__paragraph--list'}>
-                    <ul>
+                    <ol>
                         <li>Kenneth R Koedinger, Julie L Booth, and David Klahr. 2013.
                             Instructional Complexity and the Science to Constrain It.
                             Science 342, 6161: 935–937. <a className={'link'} href={'https://doi.org/10.1126/science.1238056'} target={'_blank'}>https://doi.org/10.1126/science.1238056</a> </li>
-                    </ul>
+                    </ol>
                 </div>
             </div>
 
