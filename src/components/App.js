034fd4225faba284cc1eaa2824f47e0f58e6de5e
Erik Harpstead <eharpste@cs.cmu.edu>
2021-02-07 13:46:15 -0500

    Revert "Added links to the LearnLab and expanded the About page a bit"
    This reverts commit 5b519fb05e645e5a528fe1aa17ccd4d8b6703554.

--- src/components/App.js
+++ src/components/App.js
@@ -47,13 +47,6 @@ export default class App extends Component {
                                     The OHLab at Carnegie Mellon
                                     <i className='fas fa-external-link-alt'/>
                                 </a>
-                                <a href={'https://learnlab.org/'}
-                                    className={'link sidebar__link'}
-                                    target={'_blank'}
-                                    onClick={() => this.onSetSidebarOpen(false)}>
-                                    The LearnLab at Carnegie Mellon
-                                    <i className='fas fa-external-link-alt'/>
-                                </a>
                             </div>
                         }
                         rootClassName={'navigation'}
