5749f512dbbe0b0a4a6fc0c11c32f8c0db994a8a
Qianou <qianoum@gmail.com>
2021-07-16 14:59:41 -0400

    implemented http requests and simplified card

--- src/components/App.js
+++ src/components/App.js
@@ -2,89 +2,25 @@ import React, { Component } from 'react';
 import Card from './Card.js';
 import principles from '../principles.json';
 import Button from './Button.js';
-
-function compareStrings(a, b) {
-    return (a < b) ? -1 : (a > b) ? 1 : 0;
-}
-
+import ReactGA from 'react-ga';
 export default class App extends Component {
 
     constructor() {
         super();
         this.state = {
             cards: [],
-            allFlipped: false
+            userRole: ''
         };
 
-        this.flipAll = this.flipAll.bind(this);
         this.flipCard = this.flipCard.bind(this);
-        this.sortAZ = this.sortAZ.bind(this);
-        this.sortNumerical = this.sortNumerical.bind(this);
-        this.resetCards = this.resetCards.bind(this);
-        this.shuffleCards = this.shuffleCards.bind(this);
+        this.logUserID = this.logUserID.bind(this);
     }
 
     componentDidMount() {
         this.setState({cards: principles});
     }
 
-    //utility
-    shuffle(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
-
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-
-            // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-
-        return array;
-    }
-
-    //card actions
-    flipAll() {
-        let items = this.state.cards;
-        for (let card of items) {
-            card.flipped = !this.state.allFlipped;
-        }
-        this.setState({cards: items});
-        this.setState({allFlipped: !this.state.allFlipped});
-
-        console.log(this.state);
-    }
-
-    sortNumerical() {
-        let items = principles;
-        items.sort(function(a, b){return (a.id - b.id);});
-        this.setState({cards: items});
-    }
-
-    sortAZ() {
-        let items = principles;
-        items.sort(function(a, b) {
-            return compareStrings(a.principle, b.principle);
-        });
-        this.setState({cards: items});
-    }
-
-    resetCards() {
-        let items = principles;
-        items.sort(function(a, b){return (a.id - b.id);});
-        for (let card of items) {
-            card.flipped = false;
-        }
-        this.setState({cards: items});
-    }
-
     flipCard(cardid) {
-        console.log('flip');
         let items = this.state.cards;
         for (let card of items) {
             if (card.id === cardid) {
@@ -92,39 +28,39 @@ export default class App extends Component {
             }
         }
         this.setState({cards: items});
-
-        console.log(this.state);
     }
 
-    shuffleCards() {
-        let shuffled = this.shuffle(principles);
-        this.setState({cards: shuffled});
+    initReactGA() {
+        ReactGA.initialize('UA-186459299-1'), { //G-YJEZFDTWH3; new: G-PKCB14SVPQ
+            debug: true,
+            titleCase: false,
+            gaOptions: {
+                siteSpeedSampleRate: 100
+            }
+        };
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+
+    logUserID() {
+        let id = window.prompt('Enter your assigned user id');
+        this.setState({userRole: id}); 
+        console.log(this.state.userRole);
     }
 
     render() {
+        this.initReactGA();
         return (
             <div className='cards'>
-
-                <div className={'row'}>
-                    <div className={'col-12'}>
-                        <div className={'toolbar'}>
-                            <Button onClick={this.resetCards} text={'Reset'} icon={'undo-alt'}></Button>
-                            <Button onClick={this.flipAll} text={'Flip All'} icon={'exchange-alt'}></Button>
-                            <Button onClick={this.sortAZ} text={'Sort A-Z'} icon={'sort-alpha-down'}></Button>
-                            <Button onClick={this.sortNumerical} text={'Sort Numeric'} icon={'sort-numeric-down'}></Button>
-                            <Button onClick={this.shuffleCards} text={'Shuffle'} icon={'random'}></Button>
-                        </div>
-                    </div>
-                </div>
+                <Button onClick={this.logUserID} text={'Login'}></Button>
 
                 <div className={'row'}>
 
                     {this.state.cards.map( card => (
                         <div key={card.id} className={'col-xs-12 col-sm-12 card-container'} onClick={() => this.flipCard(card.id)}>
                             <Card
+                                userRole={this.state.userRole}
                                 flipped={card.flipped}
                                 id={card.id}
-                                image={card.id}
                                 category={card.categoryId}
                                 categoryName={card.category}
                                 title={card.principle}
@@ -132,11 +68,6 @@ export default class App extends Component {
                                 descr={card.description}
                                 comparison={card.subtitle}
                                 example={card.examples}
-                                gameEx={card.exampleGame}
-                                gameExURL={card.exampleGameUrl}
-                                gameExDesc={card.exampleGameDesc}
-                                related={card.related}
-                                citation={card.cited}
                             />
                         </div>
                     ))}
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from 'react-bootstrap';
+import ReactGA from 'react-ga';
 
+function sendData(data) {
+    var request = new XMLHttpRequest();
+    request.open('POST', 'http://localhost:8000');
+    request.setRequestHeader('Access-Control-Allow-Origin', '*');
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    request.send(JSON.stringify(data));
+    console.log(data);
+}
 class Card extends React.Component {
     constructor(props) {
         super(props);
@@ -11,65 +20,120 @@ class Card extends React.Component {
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
 
+        this.timer = 0;
+        this.startTimer = this.startTimer.bind(this);
+        this.incrTime = this.incrTime.bind(this);
+        this.resetTimer = this.resetTimer.bind(this);
+
         this.state = {
             showModal: false,
+            seconds: 0
         };
+    }
+
+    incrTime() {
+        const seconds = this.state.seconds + 1;
+        this.setState({ seconds: seconds });
+    }
+
+    startTimer() {
+        if (this.timer == 0 && this.state.seconds == 0) {
+            this.timer = setInterval(this.incrTime, 1000);
+        }
+    }
+
+    resetTimer() {
+        console.log(this.state.seconds, 'seconds passed for timer', this.timer);
+        clearInterval(this.timer);
+        this.setState({ seconds: 0 });
     }
 
     handleClose() {
         this.setState({ show: false });
+        var data = {category: this.props.title,
+            action: 'Close',
+            value: this.state.seconds,
+            time: new Date().toUTCString(),
+            label: this.props.userRole
+        };
+        ReactGA.event(data);
+        sendData(data);
+
+        this.resetTimer();
     }
 
     handleShow() {
         this.setState({ show: true });
+        this.startTimer();
+        var data = {category: this.props.title,
+            action: 'Open',
+            value: 0,
+            time:  new Date().toUTCString(),
+            label: this.props.userRole
+        };
+        ReactGA.event(data);
+        sendData(data);
     }
 
     renderCard() {
         return (
-            (this.props.flipped) ?
-                <div className={'card__face card__face--front'}>
-                    <div className={'card__face--front__header'} onClick={this.handleShow}>
-                        <button className={'btn btn-link card__face--front__header__open-link'}>
-                            <i className="fas fa-external-link-alt"></i>
-                        </button>
-                        <div className={'principle-number'}>
-                            {this.props.id}
-                        </div>
-                        <div className={'principle-title'}>
-                            {this.props.title}
-                        </div>
+            <div className={'card__face card__face--front'}>
+                <div className={'card__face--front__header'} onClick={this.handleShow}>
+                    <button className={'btn btn-link card__face--front__header__open-link'}>
+                        <i className="fas fa-external-link-alt"></i>
+                    </button>
+                    <div className={'principle-number'}>
+                        {this.props.id}
                     </div>
-                    <div className={'card__face--front__category'}>
-                        <div className={'card__face--front__category__text'}>{this.props.categoryName}</div>
-                    </div>
-                    <div className={'card__face--front__body'}>
-                        <div className={'card__face--front__body__comparison'}>
-                            {this.props.comparison}
-                        </div>
-                        <div style={{backgroundImage: 'url(' + this.image + ')'}} className={'card__face--front__body__image'}></div>
-                        <div className={'card__face--front__body__example'}>
-                            {this.props.example}
-                        </div>
-
+                    <div className={'principle-title'}>
+                        {this.props.title}
                     </div>
                 </div>
-                :
-                <div className={'card__face card__face--back'}>
-                    <div className={'card__face--back__content'}>
-                        <div className={'principle-number'}>
-                            {this.props.id}
-                        </div>
-                        <div className={'principle-image'} style={{backgroundImage: 'url(' + this.image + ')'}}></div>
-                        <div className={'principle-title'}>
-                            {this.props.title}
-                        </div>
+                <div className={'card__face--front__category'}>
+                    <div className={'card__face--front__category__text'}> </div>
+                </div>
+                <div className={'card__face--front__body'}>
+                    <div className={'card__face--front__body__comparison'}>
+                        {this.props.comparison}
                     </div>
-                    <div className={'card__face--back__category'}>
-                        {this.props.categoryName}
-                    </div>
+                    <div style={{backgroundImage: 'url(' + this.image + ')'}} className={'card__face--front__body__image'}></div>
 
                 </div>
+            </div>
         );
+    }
+
+    renderDescription() {
+        if (this.props.descr) {
+            return (
+                <div>
+                    <h3>Description</h3>
+                    <p>{this.props.descr}</p>
+                </div>            
+            );
+        }
+    }
+
+    renderQuestions() {
+        if (this.props.questions) {
+            return (
+                <div className={'card-modal__details__lines'}>
+                    <h3>Questions</h3>
+                    <p>{this.props.questions}</p>
+                </div>        
+            );
+        }
+    }
+    
+    renderExamples() {
+        if (this.props.example) {
+            return (
+                <div className={'card-modal__details__lines'}>
+                    <h3>Examples</h3>
+                    <p>{this.props.example}</p>
+                </div>        
+            );
+        }
     }
 
     render() {
@@ -81,39 +145,17 @@ class Card extends React.Component {
                     <Modal.Header className={'card-modal__header'} closeButton>
                         <div className={'card-modal__header__id principle-number'}>{this.props.id}</div>
                         <h2 className={'card-modal__header__title'}>{this.props.title}</h2>
-                        <h2 className={'card-modal__header__category'}>{this.props.categoryName}</h2>
                     </Modal.Header>
                     <Modal.Body className='card-modal__body-container'>
                         <div className="row">
-                            <div className={'card-modal__summary'}>
-                                <h2>Summary view</h2>
-                            </div>
                             <div className={'card-modal__details'}>
                                 <div>
-                                    <h3>this is the extra info</h3>
+                                    <h3>Principle</h3>
+                                    <p>{this.props.comparison}</p>
                                 </div>
-                                <div>
-                                    <h3>Questions</h3>
-                                    <p>{this.props.questions}</p>
-                                </div>
-                                <div>
-                                    <h3>Summary</h3>
-                                    <p>{this.props.descr}</p>
-                                </div>
-                                <div>
-                                    <h3>Game Example</h3>
-                                    <h4>{this.props.gameEx}</h4>
-                                    <p>{this.props.gameExURL}</p>
-                                    <p>{this.props.gameExDesc}</p>
-                                </div>
-                                <div>
-                                    <h3>Related</h3>
-                                    <p>{this.props.related}</p>
-                                </div>
-                                <div>
-                                    <h3>Cited</h3>
-                                    <p>{this.props.citation}</p>
-                                </div>
+                                {this.renderDescription()}
+                                {this.renderQuestions()}
+                                {this.renderExamples()}
                             </div>
                         </div>
                     </Modal.Body>
@@ -124,6 +166,7 @@ class Card extends React.Component {
 }
 
 Card.propTypes = {
+    userRole: PropTypes.any,
     flipped: PropTypes.any,
     id: PropTypes.any,
     category: PropTypes.any,
@@ -133,11 +176,6 @@ Card.propTypes = {
     descr: PropTypes.any,
     comparison: PropTypes.any,
     example: PropTypes.any,
-    gameEx: PropTypes.any,
-    gameExURL: PropTypes.any,
-    gameExDesc: PropTypes.any,
-    related: PropTypes.any,
-    citation: PropTypes.any,
 };
 
 export default Card;
